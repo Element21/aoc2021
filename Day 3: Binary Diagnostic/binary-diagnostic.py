7afd6d82e0dd2bf122a223236f039c62d85450ca
Aiden <aiden@alanmarian.com>
2021-12-20 15:01:48 -0500

    Day 3 completed

--- Day 3: Binary Diagnostic/binary-diagnostic.py
+++ Day 3: Binary Diagnostic/binary-diagnostic.py
@@ -1,22 +1,33 @@
-from math import gamma
+with open("input.txt", "r") as f:
+    data = f.read().splitlines()
 
-
-data = open("./input.txt").read().splitlines()
+# data = """00100
+# 11110
+# 10110
+# 10111
+# 10101
+# 01111
+# 00111
+# 11100
+# 10000
+# 11001
+# 00010
+# 01010""".splitlines()
 
 
 def get_column(column, data=data):
-    "Return a generator that returns all numbers in column 'column'"
-    for i, _ in enumerate(data):
-        yield data[i][column]
+    "Return a string that includes all numbers in column 'column'"
+    return "".join([data[i][column] for i, _ in enumerate(data)])
 
 
 def pt1(data=data):
     gamma = []
     epsilon = []
-    for colnum in range(12):
-        for i in list(get_column(colnum)):
-            num0s = i.count("0")
-            num1s = i.count("1")
+    num_cols = len(data[0])
+    for colnum in range(num_cols):
+        i = get_column(colnum)
+        num0s = i.count("0")
+        num1s = i.count("1")
         if num0s > num1s:
             "0 is more common, so gamma bit is 0 and epsilon bit is 1"
             gamma.append("0")
@@ -29,51 +40,54 @@ def pt1(data=data):
 
 
 gamma, epsilon = pt1()
-print(int("".join(epsilon), 2) * int("".join(gamma), 2))
+print("part 1=", int("".join(epsilon), 2) * int("".join(gamma), 2))
+
+
+def check_bit(i, col, val):
+    # return true if bit col in i is equal to val
+    return i[col] == val
 
 
 def part2_oxygen(data=data, column=0):
     if len(data) == 1:
-        return data
-    num0s = 0
-    num1s = 0
-    for cs in list(get_column(column)):
-        num0s += cs.count("0")
-        num1s += cs.count("1")
+        return int(data[0], 2)
+    cs = get_column(column, data)
+    num0s = cs.count("0")
+    num1s = cs.count("1")
+
     if num0s > num1s:
-        "0 is more common, keep everything with a 0 in 'column' column"
-        oxygen = [j for i, j in enumerate(data) if data[i][column] == "0"]
-        return part2_oxygen(data=oxygen, column=column + 1)
-    if num0s < num1s:
-        "1 is more common, keep everything with a 1 in 'column' column"
-        oxygen = [j for i, j in enumerate(data) if data[i][column] == "1"]
-        return part2_oxygen(data=oxygen, column=column + 1)
+        # keep zeros in this bit position
+        keep = "0"
     else:
-        "Equal frequency, for oxygen, keep everything with a 1 in 'column' column"
-        oxygen = [j for i, j in enumerate(data) if data[i][column] == "1"]
-        return part2_oxygen(data=oxygen, column=column + 1)
+        keep = "1"
+    print(f"COL {column} {data} ({num0s}/{num1s}) keeping {keep}")
+    f_expr = lambda x: check_bit(x, column, val=keep)
+    return part2_oxygen(data=list(filter(f_expr, data)), column=column + 1)
 
 
 def part2_co2(data=data, column=0):
     if len(data) == 1:
-        return data
-    num0s = 0
-    num1s = 0
-    for cs in list(get_column(column)):
-        num0s += cs.count("0")
-        num1s += cs.count("1")
-    if num0s < num1s:
-        "0 is less common, keep everything with a 0 in 'column' column"
-        co2 = [j for i, j in enumerate(data) if data[i][column] == "0"]
-        return part2_oxygen(data=co2, column=column + 1)
+        return int(data[0], 2)
+    cs = get_column(column, data)
+    num0s = cs.count("0")
+    num1s = cs.count("1")
+
     if num0s > num1s:
-        "1 is less common, keep everything with a 1 in 'column' column"
-        co2 = [j for i, j in enumerate(data) if data[i][column] == "1"]
-        return part2_oxygen(data=co2, column=column + 1)
+        # keep ones in this bit position
+        keep = "1"
     else:
-        "Equal frequency, for co2, keep everything with a 0 in 'column' column"
-        co2 = [j for i, j in enumerate(data) if data[i][column] == "0"]
-        return part2_oxygen(data=co2, column=column + 1)
+        keep = "0"
+    print(f"COL {column} {data} ({num0s}/{num1s}) keeping {keep}")
+    f_expr = lambda x: check_bit(x, column, val=keep)
+    return part2_co2(data=list(filter(f_expr, data)), column=column + 1)
 
 
-print(int(part2_co2()[0], 2) * int(part2_oxygen()[0], 2))
+print("part 2")
+
+oxy = part2_oxygen(data)
+print("oxy = ", oxy)
+
+co2 = part2_co2(data)
+print("co2 = ", co2)
+
+print("part2 -- ", oxy * co2)
